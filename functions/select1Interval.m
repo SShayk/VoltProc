58dcaf7ca77fcb8177cd6e8d957076ae31bf14f5
Sarah <sshayk@bu.edu>
2026-03-25 16:40:57 -0400

    make height of interval shading the same as the Fraw plot ylims, even if zoomed in on a single trace when selecting the interval

--- functions/select1Interval.m
+++ functions/select1Interval.m
@@ -1,7 +1,7 @@
-function [interval, shadehandle, linehandles] = select1Interval(axHandle)
+function [interval, shadehandle, linehandles] = select1Interval(axHandle, yLim)
     % select1Interval - Interactively select x-axis intervals on a plot
     %
-    % Usage: interval = select1Interval(axHandle)
+    % Usage: interval = select1Interval(axHandle) (ylim argument optional)
     %
     % Left-click to mark interval boundaries (pairs of clicks)
     % Right-click or press Enter when done
@@ -9,9 +9,13 @@ function [interval, shadehandle, linehandles] = select1Interval(axHandle)
     % Returns: intervals - Nx2 matrix where each row is [x_start, x_end]
     %          shadehandle: handle to area object for current interval
     %          linehandles: handle to line objects for either side of interval
-
+    %  
     if nargin < 1
         axHandle = gca;
+    end
+
+    if nargin <2
+        yLim = ylim(axHandle);
     end
     
     axes(axHandle); % Make this the current axes
@@ -21,7 +25,7 @@ function [interval, shadehandle, linehandles] = select1Interval(axHandle)
     [x, ~, ~] = ginput(2);
     
     % Plot vertical line at click point
-    yLim = ylim(axHandle);
+    
     linehandles(1) = plot(axHandle, [x(1) x(1)], yLim, 'r--', 'LineWidth', 1,'HandleVisibility','off');
     linehandles(2) =plot(axHandle, [x(2) x(2)], yLim, 'r--', 'LineWidth', 1,'HandleVisibility','off');
         
